6436dd7bae2909d21f7b8c020fae66d1408d2875
TOANBN-PC\TOANBN <tedu.international@gmail.com>
2016-10-28 07:25:42 +0700

    Declaring class

---
+++ lesson17.ts
@@ -0,0 +1,10 @@
+class Person{
+    run(){
+        console.log('Person is running...')
+    }
+}
+
+let p1 = new Person();
+
+console.log(p1.run === Person.prototype.run);
+//console.log(p1.run());
